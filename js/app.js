670ecdc01d41d7f67b68072b5e01caa8aefd673d
Racknar0 <racknarow1@gmail.com>
2022-03-01 16:46:06 -0500

    Consult Server

--- js/app.js
+++ js/app.js
@@ -11,13 +11,59 @@ function guardarCliente () {
     const mesa = document.querySelector('#mesa').value;
     const hora = document.querySelector('#hora').value;
 
-    // Revisar campos vacios
+
+    //! VALDATION METHOD
     const camposVacios = [mesa , hora].some( campo => campo === '' );
 
     if(camposVacios) {
-        console.log('Si hay almenos un campo vacio');
-    } else {
-        console.log(' todos los campos estan llenos');
-    }
 
+        //!verificar alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son Obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove()
+            }, 3000);
+        } 
+        return;
+    } 
+
+    //! asignar datos form a cliente
+    cliente = {...cliente, mesa, hora }
+
+    //! ocultar mondal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBoostrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBoostrap.hide();
+   
+    //!mostrar las secciones
+    mostrarSecciones();
+
+    //!Obtener platillos API Json Server
+    obtenerPlatillos();
+
+}
+
+
+function mostrarSecciones() {
+    
+    const seccionesOculas = document.querySelectorAll('.d-none');
+    seccionesOculas.forEach( seccion => {
+        seccion.classList.remove('d-none')
+    })
+}
+
+function obtenerPlatillos () {
+    
+    const url = 'http://localhost:4000/platillos'
+
+    fetch ( url )
+        .then( respuesta => respuesta.json() )
+        .then( resultado => console.log(resultado) )
+        .catch( error => console.log(error))
 }
