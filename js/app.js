d0b8339d4bf98206f5bc4a1fec69bd0261873872
Racknar0 <racknarow1@gmail.com>
2022-03-01 18:45:26 -0500

    add conditions

--- js/app.js
+++ js/app.js
@@ -1,83 +1,77 @@
 let cliente = {
     mesa: '',
     hora: '',
-    pedido: []
-}
+    pedido: [],
+};
 
 const categorias = {
     1: 'Comida',
     2: 'Bebidas',
-    3: 'Postres'
-}
+    3: 'Postres',
+};
 
-const  btnGuardarCliente = document.querySelector('#guardar-cliente');
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
-function guardarCliente () {
+function guardarCliente() {
     const mesa = document.querySelector('#mesa').value;
     const hora = document.querySelector('#hora').value;
 
-
     //! VALDATION METHOD
-    const camposVacios = [mesa , hora].some( campo => campo === '' );
+    const camposVacios = [mesa, hora].some((campo) => campo === '');
 
-    if(camposVacios) {
-
+    if (camposVacios) {
         //!verificar alerta
         const existeAlerta = document.querySelector('.invalid-feedback');
 
-        if(!existeAlerta) {
+        if (!existeAlerta) {
             const alerta = document.createElement('DIV');
             alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
             alerta.textContent = 'Todos los campos son Obligatorios';
             document.querySelector('.modal-body form').appendChild(alerta);
 
             setTimeout(() => {
-                alerta.remove()
+                alerta.remove();
             }, 3000);
-        } 
+        }
         return;
-    } 
+    }
 
     //! asignar datos form a cliente
-    cliente = {...cliente, mesa, hora }
+    cliente = { ...cliente, mesa, hora };
 
     //! ocultar mondal
     const modalFormulario = document.querySelector('#formulario');
     const modalBoostrap = bootstrap.Modal.getInstance(modalFormulario);
     modalBoostrap.hide();
-   
+
     //!mostrar las secciones
     mostrarSecciones();
 
     //!Obtener platillos API Json Server
     obtenerPlatillos();
-
 }
-
 
 function mostrarSecciones() {
-    
     const seccionesOculas = document.querySelectorAll('.d-none');
-    seccionesOculas.forEach( seccion => {
-        seccion.classList.remove('d-none')
-    })
+    seccionesOculas.forEach((seccion) => {
+        seccion.classList.remove('d-none');
+    });
 }
 
-function obtenerPlatillos () {
-    
-    const url = 'http://localhost:4000/platillos'
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos';
 
-    fetch ( url )
-        .then( respuesta => respuesta.json() )
-        .then( resultado => mostrarPlatillos(resultado) )
-        .catch( error => console.log(error))
+    fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((resultado) => mostrarPlatillos(resultado))
+        .catch((error) => console.log(error));
 }
 
-function mostrarPlatillos (platillos) {
+function mostrarPlatillos(platillos) {
     const contenido = document.querySelector('#platillos .contenido');
 
-    platillos.forEach( platillo => {
+    platillos.forEach((platillo) => {
         const row = document.createElement('DIV');
         row.classList.add('row', 'py-3', 'border-top');
 
@@ -86,12 +80,12 @@ function mostrarPlatillos (platillos) {
         nombre.textContent = platillo.nombre;
 
         const precio = document.createElement('DIV');
-        precio.classList.add('col-md-3', 'fw-bold');    
+        precio.classList.add('col-md-3', 'fw-bold');
         precio.textContent = `$${platillo.precio}`;
 
         const categoria = document.createElement('DIV');
-        categoria.classList.add('col-md-3', 'fw-bold');  
-        categoria.textContent = categorias[ platillo.categoria ];
+        categoria.classList.add('col-md-3', 'fw-bold');
+        categoria.textContent = categorias[platillo.categoria];
 
         const inputCantidad = document.createElement('INPUT');
         inputCantidad.type = 'number';
@@ -101,11 +95,11 @@ function mostrarPlatillos (platillos) {
         inputCantidad.classList.add('form-control');
 
         //! fn que detecta cantidad y platillo
-        inputCantidad.onchange = function( ) {
+        inputCantidad.onchange = function () {
             const cantidad = parseInt(inputCantidad.value);
 
-            agregarPlatillo ({...platillo, cantidad}) //! spread para crear obj+cantidad junto
-        }
+            agregarPlatillo({ ...platillo, cantidad }); //! spread para crear obj+cantidad junto
+        };
 
         const agregar = document.createElement('DIV');
         agregar.classList.add('col-md-2');
@@ -114,21 +108,37 @@ function mostrarPlatillos (platillos) {
         row.appendChild(nombre);
         row.appendChild(precio);
         row.appendChild(categoria);
-        row.appendChild(agregar)
+        row.appendChild(agregar);
 
         contenido.appendChild(row);
-    })
+    });
 }
 
-function agregarPlatillo (producto) {
+function agregarPlatillo(producto) {
     let { pedido } = cliente;
 
     //Revisar que la cantidad es mayor a 0
-    if(producto.cantidad > 0) { 
-
-        cliente.pedido = [...pedido, producto]
-        
-        console.log(cliente.pedido);
+    if (producto.cantidad > 0) {
+        //Comprueba si el elemento ya existe en el array
+        if (pedido.some((articulo) => articulo.id === producto.id)) {
+            //El articulo existe se actualiza la cantidad
+            const pedidoActualizado = pedido.map( articulo => {
+                if ( articulo.id === producto.id ) {
+                    articulo.cantidad = producto.cantidad
+                }
+                return articulo;
+            } );
+            //Se asigna el nuevo array a cliente.pedido
+            cliente.pedido = [...pedidoActualizado];
+        } else {
+            //El articulo no existe se agrega al array de pedido
+            cliente.pedido = [...pedido, producto];
+        }
     } else {
-        console.log('no es mayor a cero')}
+        //Eliminar elementos cuando la cantidad es cero
+        const resultado = pedido.filter( articulo => articulo.id !== producto.id );
+        cliente.pedido = [...resultado]
+    }
+
+    console.log(cliente.pedido);
 }
