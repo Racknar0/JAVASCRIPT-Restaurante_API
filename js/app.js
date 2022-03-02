580b46c41727fa9e532f7341212e97c58468320a
Racknar0 <racknarow1@gmail.com>
2022-03-01 22:44:13 -0500

    Add Functions

--- js/app.js
+++ js/app.js
@@ -182,11 +182,74 @@ function actualizarResumen() {
     heading.classList.add('my-4', 'text-center');
 
     // Iterar sobre el array de pedidos
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
+    const {pedido} = cliente;
+    pedido.forEach( articulo => {
+        const {nombre, cantidad, precio, id} = articulo;
+
+        const lista = document.createElement('LI');
+        lista.classList.add('list-group-item');
+
+        const nombreEl = document.createElement('H4');
+        nombreEl.classList.add('my-4');
+        nombreEl.textContent = nombre;
+
+
+        //Cantidad del articulo
+        const cantidadEl = document.createElement('P');
+        cantidadEl.classList.add('fw-bold');
+        cantidadEl.textContent = 'Cantidad: ';
+
+        const cantidadValor = document.createElement('SPAN')
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+
+        //Precio del articulo
+        const precioEl = document.createElement('P');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = 'Precio: ';
+
+        const precioValor = document.createElement('SPAN')
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = `$${precio}`;
+
+
+        //Subtotal del articulo
+        const subtotalEl = document.createElement('P');
+        subtotalEl.classList.add('fw-bold');
+        subtotalEl.textContent = 'Subtotal: ';
+
+        const subtotalValor = document.createElement('SPAN')
+        subtotalValor.classList.add('fw-normal');
+        subtotalValor.textContent = calcularSubtotal(precio, cantidad);        
+
+
+        //Agregar Valores a sus contenedores
+        cantidadEl.appendChild(cantidadValor);
+        precioEl.appendChild(precioValor);
+        subtotalEl.appendChild(subtotalValor);
+
+
+        //Agregar elementos al LI
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEl);
+        lista.appendChild(precioEl);
+        lista.appendChild(subtotalEl);
+
+
+        //Agregar lista al grupo PPAL
+        grupo.appendChild(lista)
+
+    });
+
 
     //Agregar el contenido
     resumen.appendChild(mesa);
     resumen.appendChild(hora);
     resumen.appendChild(heading);
+    resumen.appendChild(grupo);
 
     contenido.appendChild(resumen);
 }
@@ -197,4 +260,8 @@ function limpiarHTML() {
     while ( contenido.firstChild ){
         contenido.removeChild(contenido.firstChild)
     }
+}
+
+function calcularSubtotal(precio, cantidad) {
+    return `$${precio * cantidad}`;
 }
